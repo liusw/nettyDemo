4633024147e4e39f5c1ce084998fe89e690a0c36
tianmaogen <tianmg@shishike.com>
2015-12-28 17:13:00 +0800

    初始化

---
+++ src/main/java/com/phei/netty/frame/fault/TimeClientHandler.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2012 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.phei.netty.frame.fault;
+
+import java.util.logging.Logger;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+/**
+ * @author lilinfeng
+ * @date 2014年2月14日
+ * @version 1.0
+ */
+public class TimeClientHandler extends SimpleChannelInboundHandler<ByteBuf>
+{
+
+	private static final Logger logger = Logger.getLogger(TimeClientHandler.class.getName());
+
+	private int counter;
+
+	private byte[] req;
+
+	/**
+	 * Creates a client-side handler.
+	 */
+	public TimeClientHandler()
+	{
+		req = ("QUERY TIME ORDER" + System.getProperty("line.separator")).getBytes();
+	}
+
+	@Override
+	public void channelActive(ChannelHandlerContext ctx)
+	{
+		ByteBuf message = null;
+		for (int i = 0; i < 100; i++)
+		{
+			message = Unpooled.buffer(req.length);
+			message.writeBytes(req);
+			ctx.writeAndFlush(message);
+		}
+	}
+
+	@Override
+	protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception
+	{
+		ByteBuf buf = (ByteBuf) msg;
+		byte[] req = new byte[buf.readableBytes()];
+		buf.readBytes(req);
+		String body = new String(req, "UTF-8");
+		System.out.println("Now is : " + body + " ; the counter is : " + ++counter);
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
+	{
+		// 释放资源
+		logger.warning("Unexpected exception from downstream : " + cause.getMessage());
+		ctx.close();
+	}
+
+}
---
+++ src/main/java/officialDemo/discard/DiscardServerHandler.java
@@ -0,0 +1,46 @@
+package officialDemo.discard;
+
+import java.util.Random;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * Handles a server-side channel.
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter
+{ // (1)
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg)
+	{ // (2)
+		// Discard the received data silently.
+//		((ByteBuf) msg).release(); // (3)
+		
+		//把传入的msg打印到控制台
+//		ByteBuf in = (ByteBuf) msg;
+//	    try {
+//	        while (in.isReadable()) { // (1)
+//	            System.out.print((char) in.readByte());
+//	            System.out.flush();
+//	        }
+//	    } finally {
+//	        ReferenceCountUtil.release(msg); // (2)
+//	    }
+		
+		//Echo Server
+		ctx.write(msg); // (1)
+		ctx.flush(); // (2)
+//		Random r = new Random();
+//		if(r.nextBoolean())
+//			ctx.flush(); // (2)
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
+	{ // (4)
+		// Close the connection when an exception is raised.
+		cause.printStackTrace();
+		ctx.close();
+	}
+}
